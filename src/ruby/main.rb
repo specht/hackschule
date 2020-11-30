f1e04c9fcaecc5a536d12d6f10cd3d2507e84571
Michael Specht <micha.specht@gmail.com>
2020-11-30 11:10:09 +0100

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -2194,6 +2194,9 @@ class Main < Sinatra::Base
             sha1 = parts[3]
             if @@tasks.include?(slug)
                 task = @@tasks[slug]
+                if sha1.include?(/[^0-9a-z]/)
+                    redirect "#{WEB_ROOT}/task/#{slug}/#{sha1.gsub(/[^0-9a-z]/, '')}", 302
+                end
             else
                 status 404
                 return

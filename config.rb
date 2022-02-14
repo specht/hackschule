7d58ca214a212af8940bd44abc87c7b04bd82e93
Michael Specht <micha.specht@gmail.com>
2022-02-14 08:51:50 +0100

    yay

--- config.rb
+++ config.rb
@@ -143,8 +143,7 @@ if PROFILE.include?(:dynamic)
         :build => './docker/pysandbox',
         :entrypoint =>  '/usr/bin/tail -f /dev/null',
         :volumes => ["#{RAW_FILES_PATH}/sandbox:/sandbox"],
-        :links => ['pixelflut:pixelflut', 'canvas:canvas', 'mysql:mysql'],
-        :ports => ['22300:22399']
+        :links => ['pixelflut:pixelflut', 'canvas:canvas', 'mysql:mysql']
     }
     docker_compose[:services][:pixelflut] = {
         :build => './docker/pixelflut',

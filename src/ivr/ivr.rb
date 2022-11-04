1df9ee431e61c0ba5ba2751e23c1858c99822b61
Michael Specht <micha.specht@gmail.com>
2022-11-04 13:37:52 +0100

    yay

--- src/ivr/ivr.rb
+++ src/ivr/ivr.rb
@@ -41,10 +41,10 @@ class Main < Sinatra::Base
     end
 
     def self.kill_call(call_id)
-        @@info_for_call_id[call_id][:stdout].close
         @@info_for_call_id[call_id][:thread].kill
         @@info_for_call_id[call_id][:notify][1].puts("hey")
         @@info_for_call_id.delete(call_id)
+        @@watcher_ping[1].puts("hey")
     end
 
     configure do

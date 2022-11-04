4c929e1a9eae2a3bb7ffafba00c5266b0221a44f
Michael Specht <micha.specht@gmail.com>
2022-11-04 17:31:39 +0100

    yay

--- src/ivr/ivr_entry.py
+++ src/ivr/ivr_entry.py
@@ -11,7 +11,9 @@ class Game(AnswerPhone):
         self.sleep(2000)
         available_codes = [os.path.basename(x) for x in glob.glob('/ivr/live/*')]
         if len(available_codes) == 0:
-            self.say("Momentan sind keine Spiele live geschaltet.")
+            self.say("Momentan sind keine Spiele verfügbar.")
+            self.sleep(2000)
+            self.say("Wenn du das ändern möchtest, schreib dein eigenes Spiel unter w, w, w. hackschule. d, e.")
             self.sleep(20000)
             return
         while True:

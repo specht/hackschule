e24c55a67b7906ce50150efcbd3c3f301342de1b
Michael Specht <micha.specht@gmail.com>
2022-11-05 19:07:50 +0100

    yay

--- src/ivr/ivr.rb
+++ src/ivr/ivr.rb
@@ -63,27 +63,26 @@ class Main < Sinatra::Base
         @@watcher_ping = IO.pipe
         @@call_id_for_stdout_fd = {}
         Thread.new do
-            STDERR.puts "I'm a watcher!"
+            STDERR.puts "[WATCHER] I'm a watcher thread!"
             loop do
                 read_sockets = []
                 read_sockets << @@watcher_ping[0]
                 @@info_for_call_id.each_pair do |call_id, info|
                     read_sockets << info[:stdout]
                 end
-                STDERR.puts "Waiting on #{read_sockets.size} sockets"
+                STDERR.puts "[WATCHER] Waiting on #{read_sockets.size} sockets"
                 streams = IO.select(read_sockets)
-                STDERR.puts "Got something"
+                STDERR.puts "[WATCHER] Got something"
                 streams.first.each do |io|
                     call_id = @@call_id_for_stdout_fd[io.fileno]
-                    STDERR.puts "call id: #{call_id}"
+                    STDERR.puts "[WATCHER] call id: #{call_id}"
                     if call_id
                         if io.eof?
-                            STDERR.puts "Script has finished, kill it!"
+                            STDERR.puts "[WATCHER] Script has finished, kill it!"
                             self.kill_call(call_id)
                         else
-                            STDERR.puts "Got a response for #{call_id}!"
+                            STDERR.puts "[WATCHER] Got a response for #{call_id}!"
                             buf = io.read_nonblock(1024)
-                            STDERR.puts buf
                             @@info_for_call_id[call_id][:buffer] += buf
                             self.handle_buffer_for_call(call_id)
                         end
@@ -96,15 +95,13 @@ class Main < Sinatra::Base
     end
 
     post '/ivr/' do
-        STDERR.puts "sipgate knocking!"
         form = request.body.read
         decoded_form = URI.decode_www_form(form)
         data = Hash[decoded_form]
-        STDERR.puts data.to_yaml
+        STDERR.puts "SIPGATE >> #{data.to_json}"
         call_id = data['callId']
         event = data['event']
         if event == 'newCall'
-            STDERR.puts "RECEIVED NEW_CALL from sipgate with call id #{call_id}!"
             @@info_for_call_id[call_id] = self.class.launch_script(call_id, "/app/ivr_entry.py")
             @@info_for_call_id[call_id][:last_path] = nil
             @@info_for_call_id[call_id][:buffer] = ''
@@ -112,24 +109,18 @@ class Main < Sinatra::Base
             @@watcher_ping[1].puts("hey")
         elsif event == 'dtmf'
             dtmf = data['dtmf']
-            STDERR.puts "RECEIVED DTMF from sipgate with call_id #{call_id} and dtmf = #{dtmf} (#{dtmf.class})!"
             if dtmf.empty? || dtmf == '-1'
                 self.class.kill_call(call_id)
             else
                 @@info_for_call_id[call_id][:stdin].puts(dtmf)
             end
-        else
-            STDERR.puts "RECEIVED #{event.upcase} from sipgate with call_id #{call_id}!"
         end
-        STDERR.puts "Sleep begin"
-        sleep 10.0
-        STDERR.puts "Sleep end"
         return unless @@info_for_call_id[call_id]
         Timeout::timeout(30) do
-            STDERR.puts "Waiting for answer from thread for #{call_id}..."
+            STDERR.puts "[MAIN] Waiting for answer from thread for #{call_id}..."
             sockets = IO.select([@@info_for_call_id[call_id][:notify][0]])
             return unless @@info_for_call_id[call_id]
-            STDERR.puts "Got answer from thread for #{call_id}..."
+            STDERR.puts "[MAIN] Got answer from thread for #{call_id}..."
             @@info_for_call_id[call_id][:notify][0].read_nonblock(1024)
             xml = StringIO.open do |io|
                 io.puts "<?xml version=\"1.0\" encoding=\"utf-8\"?>"

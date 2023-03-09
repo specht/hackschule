fba74ac4690960b2038606932b970ea4f3d2c3e7
Michael Specht <micha.specht@gmail.com>
2023-03-09 12:00:14 +0100

    yay

---
+++ src/ruby/themes.rb
@@ -0,0 +1,14 @@
+THEME_ORDER = [:eclipse, :clouds, :github,
+               :tomorrow, :ambiance, :dracula,
+               :gruvbox, :monokai, :twilight]
+THEMES = {
+    :eclipse  => {:ace => 'Eclipse' , :bg => '#ffffff', :fg => '#444444', :ld => 'l'},
+    :clouds   => {:ace => 'Clouds'  , :bg => '#ffffff', :fg => '#444444', :ld => 'l'},
+    :github   => {:ace => 'GitHub'  , :bg => '#ffffff', :fg => '#444444', :ld => 'l'},
+    :tomorrow => {:ace => 'Tomorrow', :bg => '#ffffff', :fg => '#444444', :ld => 'd'},
+    :ambiance => {:ace => 'Ambiance', :bg => '#112233', :fg => '#eeeeee', :ld => 'd'},
+    :dracula  => {:ace => 'Dracula' , :bg => '#112233', :fg => '#eeeeee', :ld => 'd'},
+    :gruvbox  => {:ace => 'Gruvbox' , :bg => '#112233', :fg => '#eeeeee', :ld => 'd'},
+    :monokai  => {:ace => 'Monokai' , :bg => '#112233', :fg => '#eeeeee', :ld => 'd'},
+    :twilight => {:ace => 'Twilight', :bg => '#112233', :fg => '#eeeeee', :ld => 'd'},
+}
